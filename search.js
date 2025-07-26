9a62570223a20a9b0bb5d6ebe0a8e6713b42f089
baneteasnow <81329385+baneteasnow@users.noreply.github.com>
2025-07-26 19:08:43 +0300

    .

--- search.js
+++ search.js
@@ -285,35 +285,3 @@
 		}
 	})(window)
 })()
-document.addEventListener("click", function (e) {
-	if (e.target.classList.contains("author-link")) {
-		e.preventDefault()
-		var authorName = e.target.dataset.author
-		var searchInput = document.getElementById("search-box")
-
-		if (searchInput) {
-			searchInput.value = authorName
-			searchInput.focus()
-			searchInput.dispatchEvent(new Event("keyup"))
-		} else {
-			window.location.href = "/?author=" + encodeURIComponent(authorName)
-		}
-	}
-})
-document.addEventListener("DOMContentLoaded", function () {
-	document.addEventListener("click", function (e) {
-		if (e.target.classList.contains("author-link")) {
-			e.preventDefault()
-			var authorName = e.target.dataset.author
-			var searchInput = document.getElementById("search-box")
-
-			if (searchInput) {
-				searchInput.value = authorName
-				searchInput.focus()
-				searchInput.dispatchEvent(new Event("keyup"))
-			} else {
-				window.location.href = "/?author=" + encodeURIComponent(authorName)
-			}
-		}
-	})
-})
